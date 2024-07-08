64031177e78bf8c7d14256741967869e0af855c6
zhangfengcdt <feng@wherobots.com>
2024-07-08 14:01:11 -0700

    fix the main

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,29 @@ use prollytree::storage::{InMemoryNodeStorage, NodeStorage};
 
 use std::thread::sleep;
 use std::time::Duration;
-use rand::seq::SliceRandom;
-use rand::thread_rng;
 use std::io::{self, Write};
 
 fn main() {
-    let num_keys = 20; // Variable to control the number of total pairs inserted
-
-    // Initialize random number generator
-    let mut rng = thread_rng();
+    let num_keys = 100; // Variable to control the number of total pairs inserted
 
     // Initialize storage and prolly trees
     let mut storage_increasing = InMemoryNodeStorage::<32>::default();
-    let mut storage_random = InMemoryNodeStorage::<32>::default();
+    let mut storage_reverse = InMemoryNodeStorage::<32>::default();
     let value_for_all = vec![100];
 
     let mut node_increasing: ProllyNode<32> = ProllyNode::builder()
-        .pattern(0b11)
-        .min_chunk_size(2)
+        .pattern(0b1111)
+        .min_chunk_size(4)
         .build();
 
-    let mut node_random: ProllyNode<32> = ProllyNode::builder()
-        .pattern(0b11)
-        .min_chunk_size(2)
+    let mut node_reverse: ProllyNode<32> = ProllyNode::builder()
+        .pattern(0b1111)
+        .min_chunk_size(4)
         .build();
 
     // Generate keys
-    let mut keys: Vec<Vec<u8>> = (0..num_keys).map(|i| vec![i as u8]).collect();
-    let mut keys_random = keys.clone();
-    keys_random.shuffle(&mut rng);
+    let keys: Vec<Vec<u8>> = (0..num_keys).map(|i| vec![i as u8]).collect();
+    let keys_reverse: Vec<Vec<u8>> = (0..num_keys).rev().map(|i| vec![i as u8]).collect();
 
     for i in 0..num_keys {
         // Clear the screen
@@ -42,17 +36,22 @@ fn main() {
         node_increasing.insert(keys[i].clone(), value_for_all.clone(), &mut storage_increasing, Vec::new());
         storage_increasing.insert_node(node_increasing.get_hash(), node_increasing.clone());
 
-        // Insert the next key in random order
-        node_random.insert(keys_random[i].clone(), value_for_all.clone(), &mut storage_random, Vec::new());
-        storage_random.insert_node(node_random.get_hash(), node_random.clone());
+        // Insert the next key in reverse order
+        node_reverse.insert(keys_reverse[i].clone(), value_for_all.clone(), &mut storage_reverse, Vec::new());
+        storage_reverse.insert_node(node_reverse.get_hash(), node_reverse.clone());
 
         // Print the trees
-        println!("Prolly Tree with Increasing Order:");
+        println!("Inserting key/value pairs in an increasing order:");
+        println!("Inserting key: {:?}", keys[i]);
         node_increasing.print_tree(&storage_increasing);
-        println!("\nProlly Tree with Random Order:");
-        node_random.print_tree(&storage_random);
+
+        println!("\n");
+
+        println!("Inserting key/value pairs in a reverse order:");
+        println!("Inserting key: {:?}", keys_reverse[i]);
+        node_reverse.print_tree(&storage_reverse);
 
         // Sleep for 2 seconds
-        sleep(Duration::from_secs(2));
+        sleep(Duration::from_millis(200));
     }
 }
